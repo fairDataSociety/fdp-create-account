ad65f324468386646ab24d5249964e6f844c20ae
tomicvladan <tomic.vladan.mail@gmail.com>
2022-07-25 15:33:51 +0200

    fix: fix contract address parameter

--- src/utils/ens.utils.ts
+++ src/utils/ens.utils.ts
@@ -2,9 +2,9 @@
 export function getEnsConfig(): any {
   const ensRegistry = process.env.REACT_APP_ENS_REGISTRY_ADDRESS;
   const publicResolver = process.env.REACT_APP_PUBLIC_RESOLVER_ADDRESS;
-  const subdomainRegistrar = process.env.REACT_APP_SUBDOMAIN_REGISTRAR_ADDRESS;
+  const fdsRegistrar = process.env.REACT_APP_SUBDOMAIN_REGISTRAR_ADDRESS;
 
-  if (ensRegistry && publicResolver && subdomainRegistrar) {
+  if (ensRegistry && publicResolver && fdsRegistrar) {
     return {
       ensOptions: {
         performChecks: true,
@@ -12,7 +12,7 @@ export function getEnsConfig(): any {
         contractAddresses: {
           ensRegistry,
           publicResolver,
-          subdomainRegistrar,
+          fdsRegistrar,
         },
       },
       ensDomain: "fds",
