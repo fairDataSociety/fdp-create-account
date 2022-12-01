db2c526c808a85f7a6498d798fc8919eafc6da07
tfius <tadej.fius@gmail.com>
2022-12-01 15:28:31 +0100

    Changing Send button to be larger

--- src/pages/register/waiting-payment.tsx
+++ src/pages/register/waiting-payment.tsx
@@ -2,109 +2,112 @@ import { useEffect, useRef } from "react";
 import { utils } from "ethers";
 import intl from "react-intl-universal";
 import { styled } from "@mui/system";
-import { CircularProgress, Typography } from "@mui/material";
-import { Account } from "../../model/general.types";
-import ClipboardButton from "../../components/clipboard-button/clipboard-button.component";
-import { getAccountBalance } from "../../services/account.service";
+import { CircularProgress, Typography, Button } from '@mui/material'
+import { Account } from '../../model/general.types'
+import ClipboardButton from '../../components/clipboard-button/clipboard-button.component'
+import { getAccountBalance } from '../../services/account.service'
 import {
   useAccount,
   usePrepareSendTransaction,
   useSendTransaction,
   useWaitForTransaction,
-} from "wagmi";
+} from 'wagmi'
 
 export interface WaitingPaymentProps {
-  account: Account;
-  onPaymentDetected: (balance: string) => void;
-  onError: (error: unknown) => void;
+  account: Account
+  onPaymentDetected: (balance: string) => void
+  onError: (error: unknown) => void
 }
 
-const ContainerDiv = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  margin: "20px auto 0 auto",
-});
+const ContainerDiv = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  margin: '20px auto 0 auto',
+})
 
-const networkInfo = process.env.REACT_APP_BLOCKCHAIN_INFO;
+const networkInfo = process.env.REACT_APP_BLOCKCHAIN_INFO
 
 const WaitingPayment = ({
   account,
   onPaymentDetected,
   onError,
 }: WaitingPaymentProps) => {
-  const amount = "0.01";
-  const { isConnected } = useAccount();
+  const amount = '0.01'
+  const { isConnected } = useAccount()
   const { config } = usePrepareSendTransaction({
     request: {
       to: account,
       value: utils.parseEther(amount),
     },
-  });
-  const { sendTransaction, data } = useSendTransaction(config);
+  })
+  const { sendTransaction, data } = useSendTransaction(config)
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
-  });
+  })
 
-  const timer = useRef<NodeJS.Timeout | null>();
+  const timer = useRef<NodeJS.Timeout | null>()
 
   const checkPayment = async () => {
     try {
-      const balance = await getAccountBalance(account);
+      const balance = await getAccountBalance(account)
 
       if (balance.gt(0)) {
-        closeTimer();
-        onPaymentDetected(`${utils.formatEther(balance)} ETH`);
+        closeTimer()
+        onPaymentDetected(`${utils.formatEther(balance)} ETH`)
       }
     } catch (error) {
-      console.error(error);
-      closeTimer();
-      onError(error);
+      console.error(error)
+      closeTimer()
+      onError(error)
     }
-  };
+  }
 
   const closeTimer = () => {
     if (timer.current) {
-      clearInterval(timer.current);
-      timer.current = null;
+      clearInterval(timer.current)
+      timer.current = null
     }
-  };
+  }
 
   if (isSuccess) {
-    checkPayment();
+    checkPayment()
   }
 
   useEffect(() => {
-    timer.current = setInterval(checkPayment, 15000);
+    timer.current = setInterval(checkPayment, 15000)
 
-    return closeTimer;
-  }, []);
+    return closeTimer
+  }, [])
 
   return (
     <ContainerDiv>
       {networkInfo && (
-        <Typography variant="h6" sx={{ margin: "auto" }}>
+        <Typography variant="h6" sx={{ margin: 'auto' }}>
           {networkInfo}
         </Typography>
       )}
 
-      <Typography variant="h5" sx={{ margin: "auto" }}>
+      <Typography variant="h5" sx={{ margin: 'auto' }}>
         <span data-testid="account">{account}</span>
         <ClipboardButton text={account} />
       </Typography>
 
-      <CircularProgress sx={{ margin: "auto", marginTop: "30px" }} />
+      <CircularProgress sx={{ margin: 'auto', marginTop: '30px' }} />
 
       {isConnected && (
         <>
-          <button
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
             disabled={isLoading || !sendTransaction}
             onClick={(e) => {
-              e.preventDefault();
-              sendTransaction?.();
+              e.preventDefault()
+              sendTransaction?.()
             }}
           >
-            {isLoading ? "Sending..." : "Send"}
-          </button>
+            {isLoading ? 'Sending...' : 'Send'}
+          </Button>
           {isSuccess && (
             <div>
               Successfully sent {amount} ether to {account}
@@ -116,12 +119,12 @@ const WaitingPayment = ({
       <Typography
         variant="body1"
         align="center"
-        sx={{ margin: "auto", marginTop: "30px" }}
+        sx={{ margin: 'auto', marginTop: '30px' }}
       >
-        {intl.get("DISCLAIMER")}
+        {intl.get('DISCLAIMER')}
       </Typography>
     </ContainerDiv>
-  );
-};
+  )
+}
 
 export default WaitingPayment;
